48a7b29e090758c8fcd4d91e0ac72d9bec5f95b3
acolombo09 <acolombo0911@gmail.com>
2023-07-22 17:34:16 +0200

    completed_exercise_bonus1_already-done_in_class

--- js/main.js
+++ js/main.js
@@ -38,22 +38,26 @@ const app = createApp({
         id: "",
         text: "",
         color: "#FF00FF",
+        done: false,
       },
       todoList: [
         { 
           id: 1,
           text: "prima task",
           color: "#15C632",
+          done: false,
         },
         { 
           id: 2,
           text: "seconda task",
           color: "#1562C6",
+          done: false,
         },
         { 
           id: 3,
           text: "terza task",
           color: "#66A2C7",
+          done: false,
         },
       ],
     }
@@ -75,9 +79,13 @@ const app = createApp({
     deleteTodoItem(itemId){
       // console.log(itemId);
 
+      // recupero con findindex l'elemento che corrisponde alla condizione che ho bisogno
+      // è simile alla funzione filter, di conseguenza devo ragionare per booleani
+      // esiste anche la funzione find, ma ti restituisce solo l'oggetto, non anche il suo indice
       let indexToDelete = this.todoList.findIndex((todoItem) => todoItem.id === itemId);
 
-      console.log(indexToDelete);
+      // console.log(indexToDelete);
+
       // for (let i = 0; i < this.todoList.length; i++) {
       //   const todoItem = this.todoList[i];
         
@@ -87,12 +95,22 @@ const app = createApp({
       // }
 
       // indice elemento da cancellare, quanti elementi da cancellare(1)
-      // this.todoList.splice(indexToDelete, 1)
+      this.todoList.splice(indexToDelete, 1);
 
       this.todoList[indexToDelete].deleted = true;
     },
     emptyList(){
-      this.todoList = []
-    }
+      this.todoList = [];
+      this.nextId = 1; // Reset dell'indice delle task tornando a 1
+    },
+    toggleDone(todoItem) {
+      // se il todoitem è done allora imposto lo stile, altrimenti no e viceversa
+      // nell'html quando todoItem.done è true, applica lo stile del testo sbarrato.
+      if (todoItem.done) {
+        todoItem.done = false;
+      } else {
+        todoItem.done = true;
+      }
+    },
   },
 }).mount("#app");
